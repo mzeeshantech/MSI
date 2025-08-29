333ae2429cc4eafd1ec020e432b5feff1983fdff
zeeshannaviu <zeeshan@naviu.tech>
2025-08-30 01:00:16 +0500

    new changes

--- run_app.bat
+++ run_app.bat
@@ -5,5 +5,9 @@ echo Activating virtual environment
 call venv\Scripts\activate
 echo Pulling latest changes from Git
 git pull origin master
+echo running migration
+python manage.py migrate
 echo Starting Django development server
 python manage.py runserver
+pause
+explorer "http://localhost:8000"
